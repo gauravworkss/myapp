26b0e13c391b062240dbe872668551aa535259e9
Gaurav <saxenagau@gmail.com>
2024-04-15 11:23:18 +0000

    Initial Commit

---
+++ src/Product.tsx
@@ -0,0 +1,11 @@
+const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+
+ export const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.title}
+    </li>
+  );
